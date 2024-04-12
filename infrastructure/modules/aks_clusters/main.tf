885276e0e77a5d031586a4581ad1017b69492431
TheTeeThailand <144491675+TheTeeThailand@users.noreply.github.com>
2024-04-12 17:01:28 -0400

    Add files via upload

---
+++ infrastructure/modules/aks_clusters/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.98.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_kubernetes_cluster" "aks_test" {
+  name                = "aks-test"
+  location            = "Canada Central"
+  resource_group_name = "cst8918-final-project-group-3"
+  dns_prefix          = "akstest"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  kubernetes_version = "1.29.0"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_prod" {
+  name                = "aks-prod"
+  location            = "Canada Central"
+  resource_group_name = "cst8918-final-project-group-3"
+  dns_prefix          = "aksprod"
+
+  default_node_pool {
+    name                = "default"
+    node_count          = 1
+    //vm_size             = "Standard_DS2_v2"
+    vm_size    = "Standard_DS2_v2"
+    min_count           = 1
+    max_count           = 3
+    enable_auto_scaling = true
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  kubernetes_version = "1.29.0"
+}
---
+++ infrastructure/modules/backend/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.98.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "backend_rg" {
+  name     = "cst8918-final-project-group-3"
+  location = "Canada Central"
+}
+
+resource "azurerm_storage_account" "backend_sa" {
+  name                     = "cst8919backendstorage"
+  resource_group_name      = azurerm_resource_group.backend_rg.name
+  location                 = azurerm_resource_group.backend_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+
+  tags = {
+    environment = "backend"
+  }
+}
+
+resource "azurerm_storage_container" "terraform_state" {
+  name                  = "terraform-state"
+  storage_account_name  = azurerm_storage_account.backend_sa.name
+  container_access_type = "private"
+}
+
+output "backend_rg_name" {
+  value = azurerm_resource_group.backend_rg.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.backend_sa.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.terraform_state.name
+}
